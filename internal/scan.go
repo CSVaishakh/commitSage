2a0a08f28ece4bed1fb3009fefc9964bd9bd4fac
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-12 17:21:54 +0530

    feat : implemeted the scan algoiritm to traverse throught the filee tree adn return all files present with their path

---
+++ internal/scan.go
@@ -0,0 +1,22 @@
+package internal
+
+import (
+	"io/fs"
+	"path/filepath"
+)
+func Scan(root string) ([]string,error){
+	var files []string
+
+	err := filepath.WalkDir(root,func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		
+		if !d.IsDir(){
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	return files, err
+}
